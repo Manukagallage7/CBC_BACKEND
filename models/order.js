a4e5eb3ff04cd8d2635af5321fea4fdf267fd152
Manukagallage7 <manukakavinda1110@gmail.com>
2025-06-21 17:37:06 +0530

    feat: add order model with schema definition for order management

---
+++ models/order.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose'
+
+const orderSchema = mongoose.Schema({
+    orderId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email : {
+        type: String,
+        required: true
+    },
+    orderItems: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            },
+            quantity: {
+                type: String,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    date :{
+        type: Date,
+        default: Date.now
+    },
+    total: {
+        type: Number,
+        required: true
+    },
+    paymentId: {
+        type: String,
+    },
+    notes: {
+        type: String
+    },
+    status: {
+        type: String,
+        default: "preparing"
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    }
+})
+
+const Order = mongoose.model("orders", orderSchema)
