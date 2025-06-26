39972aeefcb23b4e5b86f088397ec70058b8fc5e
Manukagallage7 <manukakavinda1110@gmail.com>
2025-06-26 15:04:41 +0530

    feat: add isAdmin and isCustomer functions for user role validation

--- controller/userController.js
+++ controller/userController.js
@@ -126,3 +126,25 @@ export function updateUser(req,res) {
         );
     });
 }
+
+export function isAdmin(req){
+    if(req.user == null){
+        return false
+    }
+
+    if(req.user.type !== "Admin"){
+        return false
+    }
+    return true
+}
+
+export function isCustomer(req){
+    if(req.user == null){
+        return false
+    }
+
+    if(req.user.type !== "Customer"){
+        return false
+    }
+    return true
+}
